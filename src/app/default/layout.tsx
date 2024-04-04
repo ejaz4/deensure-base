31948e18d7c76f7bb00b7a0b9c950392b3327da8
ejaz4 <ejazali4@protonmail.ch>
2024-04-04 20:23:51 +0100

    💄 Account for status bar

--- src/app/default/layout.tsx
+++ src/app/default/layout.tsx
@@ -1,9 +1,24 @@
+"use client";
 import { Footer } from "@/components/footer";
+import { StatusBarHeight } from "@/libs/mal";
+import { useState, useEffect } from "react";
 
 const DefaultApp = ({ children }: { children: React.ReactNode }) => {
+	const [statusBarHeight, setStatusBarHeight] = useState(0);
+
+	useEffect(() => {
+		const height = StatusBarHeight();
+		setStatusBarHeight(height);
+	}, []);
+
 	return (
 		<div>
 			<div>{children}</div>
+			<style>
+				{`:root {
+					--status-bar-height: ${statusBarHeight}px
+				}`}
+			</style>
 		</div>
 	);
 };
--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./onboarding.module.css";
+import { StatusBarHeight } from "@/libs/mal";
 
 const OnboardingLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div className={styles.onboarding}>{children}</div>;
+	const [statusBarHeight, setStatusBarHeight] = useState(0);
+
+	useEffect(() => {
+		const height = StatusBarHeight();
+		setStatusBarHeight(height);
+	}, []);
+
+	return (
+		<>
+			<div className={styles.onboarding}>{children}</div>
+			<style>
+				{`:root {
+					--status-bar-height: ${statusBarHeight}px
+				}`}
+			</style>
+		</>
+	);
 };
 
 export default OnboardingLayout;
